26736f425c43c253de43cfea09a8ec993ff025a0
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-05-25 21:36:39 -0300

    Ejercicio 4 check

--- js/ejercicios/ejercicio-4-tp2.js
+++ js/ejercicios/ejercicio-4-tp2.js
@@ -1,1 +1,23 @@
+/*4- Realiza un script que pida números hasta que se pulse “cancelar”. 
+Si no es un número deberá indicarse con un «alert» y seguir pidiendo números.
+ Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.*/
 
+ let numero=0;
+ let suma=0;
+ let cadena;
+ 
+ cadena = prompt("Ingrese un número");
+ numero = parseInt(cadena);
+ 
+ for (let i = 0; cadena != null; i++) {
+     if (isNaN(numero)) {
+         alert("Debe ingrese un valor numérico");
+       } else {
+         suma = suma + numero;
+       }
+ 
+     cadena = prompt("Ingrese un número");
+     numero = parseInt(cadena);
+ }
+ 
+ document.write(suma); 
