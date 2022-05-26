2f12678205973ac57cc838294305ece82bb278c0
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-05-26 01:32:25 -0300

    Ejercicio 12 check

--- js/ejercicios/ejercicio-11-tp2.js
+++ js/ejercicios/ejercicio-11-tp2.js
@@ -1,7 +1,7 @@
 /*11- Realiza un script que pida por teclado 3 edades 
 y 3 nombres e indique el nombre del mayor. */
 
-let edades = parseInt(prompt('Ingrese edad'));
-let nombres = prompt('Ingrese nombre');
+let edades = parseInt(prompt('Ingrese edad '));
+
 
 
