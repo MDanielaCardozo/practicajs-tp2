144bdb600947d36d0711ee1a849f943503911a25
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-05-25 21:51:42 -0300

    Ejercicio 10 check

--- js/ejercicios/ejercicio-10-tp2.js
+++ js/ejercicios/ejercicio-10-tp2.js
@@ -1,4 +1,40 @@
+/*10- Realiza un script que pida número de filas y columnas y escriba una tabla. 
+Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. 
+Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.*/
 
+let filas = parseInt(prompt("Ingrese una cantidad de filas"));
+let columnas = parseInt(prompt("Ingrese una cantidad de columnas"));
 
+let resultado = filas * columnas;
+console.log(resultado);
 
+document.write(`<table> <tbody>`);
+// aqui dibujo las filas y celdas
+for (let indiceFila = 0; indiceFila < filas; indiceFila++) {
+  // aqui dibujo las filas
+  document.write("<tr>");
+  // ahora dibujo las celdas
+  for (let indiceColumna = 0; indiceColumna < columnas; indiceColumna++) {
+    document.write(`<td>${resultado}</td>`);
+    resultado--;
+  }
 
+  document.write("</tr>");
+}
+
+document.write(`</tbody> </table>`);
+
+// document.write(`<table>
+// <tbody>
+//     <tr>
+//         <td>1</td>
+//         <td>2</td>
+//         <td>3</td>
+//     </tr>
+//     <tr>
+//         <td>1</td>
+//         <td>2</td>
+//         <td>3</td>
+//     </tr>
+// </tbody>
+// </table>`)
--- js/ejercicios/ejercicio-9-tp2.js
+++ js/ejercicios/ejercicio-9-tp2.js
@@ -1,2 +1,17 @@
+ /*9- Crea un script que escriba los números del 1 al 500, 
+que indique cuáles son múltiplos de 4 y de 9 y que cada 5 líneas muestre una línea horizontal. Por ejemplo :
+1
+2
+3
+4 (Múltiplo de 4)
+5-
+————————————————————-
+6
+7
+8 (Múltiplo de 4)
+9 (Múltiplo de 9)
+10
+*/
+
 
 
