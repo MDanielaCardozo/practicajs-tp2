aeee7b094f6bf6cc8b69fa28f15659886fea7f30
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-05-26 00:38:53 -0300

    Ejercicio 9 check

--- js/ejercicios/ejercicio-9-tp2.js
+++ js/ejercicios/ejercicio-9-tp2.js
@@ -13,5 +13,56 @@ que indique cuáles son múltiplos de 4 y de 9 y que cada 5 líneas muestre una 
 10
 */
 
+let numero;
 
+for(let i=1 ; i<=500 ; i++)
+{
+    
+    if(i%5 == 0)
+    {
+        if(i%4 == 0 || i%9 == 0)
+        {
+           if(i%4 == 0)
+           {
+               if(i%9 == 0)
+               {
+                document.write(`${i} (Múltiplo de 4 y 9 ) <br> `);  
+               }
+               else{
+                document.write(`${i} (Múltiplo de 4) <br> `);  
+               }
+           }else{
+            document.write(`${i} (Múltiplo de 9) <br> `);  
+           }
+
+        }
+        else{
+            document.write(`${i}<br> `); 
+        }
+        document.write('------------------------------------------------------<br> ');
+    }
+    else{
+       
+        if(i%4 == 0 || i%9 == 0)
+        {
+           if(i%4 == 0)
+           {
+               if(i%9 == 0)
+               {
+                document.write(`${i} (Múltiplo de 4 y 9 )<br> `);  
+               }
+               else{
+                document.write(`${i} (Múltiplo de 4)<br> `);  
+               }
+           }else{
+            document.write(`${i} (Múltiplo de 9)<br> `);  
+           }
+
+        }
+        else{
+            document.write(`${i} <br> `);
+        }
+
+    }
+}
 
