13e97a63f606ba1daca4916cac39766388d85ba2
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-06-18 17:38:39 -0300

    Ejercicio 17 check

--- js/ejercicios/ejercicio-17-tp2.js
+++ js/ejercicios/ejercicio-17-tp2.js
@@ -1,2 +1,29 @@
 /*17- Realiza un script que muestre la posición de la primera vocal 
 de un texto introducido por teclado.*/
+
+let vocales = ["a", "e", "i", "o", "u"];
+let texto = prompt("Ingrese una frase");
+// transformo el texto a minusculas
+let textoMinisculas = texto.toLowerCase();
+let posicion = 0;
+//Creo una variable bandera para detener el bucle
+let stop = false;
+
+for (let i = 0; i < textoMinisculas.length; i++) {
+  for (let j = 0; j < vocales.length; j++) {
+    if (vocales[j] === textoMinisculas.charAt(i)) {
+      posicion = i;
+      stop = true;
+      break;
+    }
+  }
+  // aqui preguntamos si stop == true, si lo es salgo del bucle
+  if (stop) {
+    break;
+  }
+}
+if(stop!= true){
+  document.write(`La palabra ingresada no tiene vocales` );
+}else{
+  document.write(`La primera vocal '${textoMinisculas.charAt(posicion)}' está en la posición ${posicion}` );
+}
