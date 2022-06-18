bf5b089051e51d992fa33be2a9421731cbc16561
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-06-18 17:33:58 -0300

    Ejercicio 15 check

--- js/ejercicios/ejercicio-15-tp2.js
+++ js/ejercicios/ejercicio-15-tp2.js
@@ -1,4 +1,20 @@
 /*15- Realiza un script que cuente el número de vocales que tiene un texto.*/
 
-let texto = prompt('Ingrese un texto');
+let cadena = prompt("Ingrese una cadena de texto:");
+cadena = cadena.toUpperCase(); //Devuelve el string en mayúsculas.
+let caracter;
+let contador = 0;
 
+for (let i = 0; i < cadena.length; i++) {
+  caracter = cadena.charAt(i); //recupera el caracter i del string.
+  if (
+    caracter == "A" ||
+    caracter == "E" ||
+    caracter == "I" ||
+    caracter == "O" ||
+    caracter == "U"
+  ) {
+    contador++;
+  }
+}
+document.write(`Número de Vocales: ${contador}`);
