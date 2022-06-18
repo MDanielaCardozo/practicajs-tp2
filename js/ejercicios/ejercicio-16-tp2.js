da4186d20f2464e0b0346203c3131fbae7ac2c5f
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-06-18 17:36:36 -0300

    Ejercicio 16 check

--- js/ejercicios/ejercicio-16-tp2.js
+++ js/ejercicios/ejercicio-16-tp2.js
@@ -1,2 +1,11 @@
 /*16- Realiza un script que pida una cadena de texto y la devuelva al revés. 
 Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”.*/
+
+let cadena = prompt("Ingrese un Texto");
+let caracter, salida = "";
+
+for (let j = 0; j < cadena.length; j++) {
+    caracter = cadena.charAt(j); //recupera el caracter de la posicion j del string.
+    salida = caracter + salida;
+}
+document.write(salida);
